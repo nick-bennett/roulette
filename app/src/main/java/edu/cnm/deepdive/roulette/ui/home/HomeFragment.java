dcec4e45b9a365c1013e289dae20907f840c01e7
Nick Bennett <nick@nickbenn.com>
2021-02-11 15:18:47 -0700

    Implemented simple number generation & display.

--- app/src/main/java/edu/cnm/deepdive/roulette/ui/home/HomeFragment.java
+++ app/src/main/java/edu/cnm/deepdive/roulette/ui/home/HomeFragment.java
@@ -3,13 +3,14 @@ package edu.cnm.deepdive.roulette.ui.home;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.roulette.R;
 
 public class HomeFragment extends Fragment {
@@ -18,14 +19,19 @@ public class HomeFragment extends Fragment {
 
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
-    homeViewModel =
-        ViewModelProviders.of(this).get(HomeViewModel.class);
+    homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
     View root = inflater.inflate(R.layout.fragment_home, container, false);
-    final TextView textView = root.findViewById(R.id.text_home);
-    homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+    final TextView rouletteValue = root.findViewById(R.id.roulette_value);
+    homeViewModel.getRouletteValue().observe(getViewLifecycleOwner(), new Observer<String>() {
       @Override
       public void onChanged(@Nullable String s) {
-        textView.setText(s);
+        rouletteValue.setText(s);
+      }
+    });
+    root.findViewById(R.id.spin_wheel).setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        homeViewModel.spinWheel();
       }
     });
     return root;
