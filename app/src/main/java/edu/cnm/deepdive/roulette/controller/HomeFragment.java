0d7195b7ab423088baf9b23d046205f00cd7a6a9
Nick Bennett <nick@nickbenn.com>
2021-02-14 06:41:58 -0700

    Use view binding.

--- app/src/main/java/edu/cnm/deepdive/roulette/controller/HomeFragment.java
+++ app/src/main/java/edu/cnm/deepdive/roulette/controller/HomeFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.animation.RotateAnimation;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,29 +13,35 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.roulette.R;
+import edu.cnm.deepdive.roulette.databinding.FragmentHomeBinding;
 import edu.cnm.deepdive.roulette.viewmodel.HomeViewModel;
 
 public class HomeFragment extends Fragment {
 
+  private FragmentHomeBinding binding;
   private HomeViewModel homeViewModel;
 
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
-    homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
-    View root = inflater.inflate(R.layout.fragment_home, container, false);
-    TextView rouletteValue = root.findViewById(R.id.roulette_value);
-    homeViewModel.getRouletteValue().observe(getViewLifecycleOwner(), new Observer<String>() {
-      @Override
-      public void onChanged(@Nullable String s) {
-        rouletteValue.setText(s);
-      }
-    });
-    root.findViewById(R.id.spin_wheel).setOnClickListener(new OnClickListener() {
+    binding = FragmentHomeBinding.inflate(inflater, container, false);
+    binding.spinWheel.setOnClickListener(new OnClickListener() {
       @Override
       public void onClick(View v) {
         homeViewModel.spinWheel();
       }
     });
-    return root;
+    return binding.getRoot();
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+    homeViewModel.getRouletteValue().observe(getViewLifecycleOwner(), new Observer<String>() {
+      @Override
+      public void onChanged(@Nullable String s) {
+        binding.rouletteValue.setText(s);
+      }
+    });
   }
 }
