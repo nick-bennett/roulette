b2c8cf11f5fe22cd93cdbb30ec3d286e1c34f453
Nick Bennett <nick@nickbenn.com>
2021-03-02 10:58:10 -0700

    Google Sign-in integration in progress.

---
+++ app/src/main/java/edu/cnm/deepdive/roulette/controller/LoginActivity.java
@@ -0,0 +1,61 @@
+package edu.cnm.deepdive.roulette.controller;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+import edu.cnm.deepdive.roulette.R;
+import edu.cnm.deepdive.roulette.databinding.ActivityLoginBinding;
+import edu.cnm.deepdive.roulette.service.GoogleSignInService;
+
+public class LoginActivity extends AppCompatActivity {
+
+  private static final int LOGIN_REQUEST_CODE = 2047;
+
+  private GoogleSignInService service;
+  private ActivityLoginBinding binding;
+
+  @SuppressLint("CheckResult")
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    service = GoogleSignInService.getInstance();
+    //noinspection ResultOfMethodCallIgnored
+    service
+        .refresh()
+        .subscribe(
+            this::updateAndSwitch,
+            (throwable) -> {
+              binding = ActivityLoginBinding.inflate(getLayoutInflater());
+              binding.signIn.setOnClickListener((v) -> service.startSignIn(this, LOGIN_REQUEST_CODE));
+              setContentView(binding.getRoot());
+            }
+        );
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    if (requestCode == LOGIN_REQUEST_CODE) {
+      service
+          .completeSignIn(data)
+          .addOnSuccessListener(this::updateAndSwitch)
+          .addOnFailureListener((throwable) -> Snackbar.make(binding.getRoot(),
+              R.string.sign_in_failure, BaseTransientBottomBar.LENGTH_INDEFINITE).show());
+    } else {
+      super.onActivityResult(requestCode, resultCode, data);
+    }
+  }
+
+  private void updateAndSwitch(GoogleSignInAccount account) {
+    // If we have a local database with a user table, update based on login.
+    startActivity(
+        new Intent(this, MainActivity.class)
+            .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK)
+    );
+  }
+
+}
