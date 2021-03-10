7028c3747d19918fd1cba494e37bd50f092f4595
Nick Bennett <nick@nickbenn.com>
2021-03-10 15:05:25 -0700

    Settings (in-progress).

---
+++ app/src/main/java/edu/cnm/deepdive/roulette/controller/SettingsFragment.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.roulette.controller;
+
+import android.os.Bundle;
+import androidx.preference.PreferenceFragmentCompat;
+import edu.cnm.deepdive.roulette.R;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+  @Override
+  public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+    setPreferencesFromResource(R.xml.preferences, rootKey);
+  }
+
+}
