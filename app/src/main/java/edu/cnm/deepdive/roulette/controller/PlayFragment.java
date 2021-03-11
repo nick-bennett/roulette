89837a0925929829fe054264bac79e73d2d621cf
Nick Bennett <nick@nickbenn.com>
2021-03-11 14:18:52 -0700

    MOved control to wheel.

--- app/src/main/java/edu/cnm/deepdive/roulette/controller/PlayFragment.java
+++ app/src/main/java/edu/cnm/deepdive/roulette/controller/PlayFragment.java
@@ -46,7 +46,7 @@ public class PlayFragment extends Fragment {
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
     binding = FragmentPlayBinding.inflate(inflater, container, false);
-    binding.spinWheel.setOnClickListener((v) -> spinWheel());
+    binding.rouletteWheel.setOnClickListener((v) -> spinWheel());
     binding.placeWager.setOnClickListener((v) -> {
       Navigation.findNavController(binding.getRoot())
           .navigate(PlayFragmentDirections.actionNavigationPlayToNavigationWager());
@@ -104,7 +104,7 @@ public class PlayFragment extends Fragment {
   private void spinWheel() {
     if (!spinning) {
       spinning = true;
-      binding.spinWheel.setEnabled(false);
+      binding.rouletteWheel.setEnabled(false);
       binding.rouletteValue.setVisibility(View.INVISIBLE);
       playViewModel.spinWheel();
     }
@@ -147,7 +147,7 @@ public class PlayFragment extends Fragment {
     public void onAnimationEnd(Animation animation) {
       binding.rouletteWheel.setRotation(finalRotation);
       spinning = false;
-      binding.spinWheel.setEnabled(true);
+      binding.rouletteWheel.setEnabled(true);
       binding.rouletteValue.setVisibility(View.VISIBLE);
     }
 
