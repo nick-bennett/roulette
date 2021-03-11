c878dfc85ad40910043baf1972961d86fa126644
Nick Bennett <nick@nickbenn.com>
2021-03-11 09:19:12 -0700

    Settings screen implementation complete.

--- app/src/main/java/edu/cnm/deepdive/roulette/controller/SettingsFragment.java
+++ app/src/main/java/edu/cnm/deepdive/roulette/controller/SettingsFragment.java
@@ -1,7 +1,10 @@
 package edu.cnm.deepdive.roulette.controller;
 
 import android.os.Bundle;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SeekBarPreference;
 import edu.cnm.deepdive.roulette.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
@@ -9,6 +12,23 @@ public class SettingsFragment extends PreferenceFragmentCompat {
   @Override
   public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
     setPreferencesFromResource(R.xml.preferences, rootKey);
+    forceStartingPotIncrement();
+  }
+
+  private void forceStartingPotIncrement() {
+    SeekBarPreference startingPot = findPreference(getString(R.string.starting_pot_key));
+    int increment = getResources().getInteger(R.integer.starting_pot_increment);
+    //noinspection ConstantConditions
+    startingPot.setOnPreferenceChangeListener((preference, newValue) -> {
+      int value = (Integer) newValue;
+      boolean save = true;
+      if (value % increment != 0) {
+        value = Math.round((float) value / increment) * increment;
+        startingPot.setValue(value);
+        save = false;
+      }
+      return save;
+    });
   }
 
 }
