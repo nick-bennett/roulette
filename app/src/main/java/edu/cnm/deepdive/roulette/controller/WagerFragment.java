1770f24a7c6f8e77a10da8337d5db972ae70ad12
Nick Bennett <nick@nickbenn.com>
2021-03-23 10:53:34 -0600

    Display of wager surface.

--- app/src/main/java/edu/cnm/deepdive/roulette/controller/WagerFragment.java
+++ app/src/main/java/edu/cnm/deepdive/roulette/controller/WagerFragment.java
@@ -1,33 +1,58 @@
 package edu.cnm.deepdive.roulette.controller;
 
 import android.os.Bundle;
+import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import edu.cnm.deepdive.roulette.R;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.GridLayoutManager.SpanSizeLookup;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import edu.cnm.deepdive.roulette.adapter.WagerSpaceAdapter;
+import edu.cnm.deepdive.roulette.databinding.FragmentWagerBinding;
+import edu.cnm.deepdive.roulette.viewmodel.PlayViewModel;
 
 public class WagerFragment extends Fragment {
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    // TODO Get arguments.
-  }
+  private static final int FULL_WIDTH = 6;
+  private static final int ZERO_SPACE_WIDTH = 3;
+  private static final int NORMAL_SPACE_WIDTH = 2;
+
+  private FragmentWagerBinding binding;
+  private PlayViewModel viewModel;
 
   @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-      Bundle savedInstanceState) {
-    // TODO Inflate using view binding.
-    return inflater.inflate(R.layout.fragment_wager, container, false);
+  public View onCreateView(
+      @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    binding = FragmentWagerBinding.inflate(inflater, container, false);
+    GridLayoutManager layoutManager =
+        new GridLayoutManager(getContext(), FULL_WIDTH, LinearLayoutManager.VERTICAL, false);
+    layoutManager.setSpanSizeLookup(new WagerSpanLookup());
+    binding.wagerSpaces.setLayoutManager(layoutManager);
+    binding.wagerSpaces.setAdapter(new WagerSpaceAdapter(getContext(),
+        (view, position, value) -> Log.d(getClass().getName(), value + " clicked"),
+        (view, position, value) -> Log.d(getClass().getName(), value + " long-pressed")
+    ));
+    return binding.getRoot();
   }
 
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    // TODO Connect to viewmodel.
+    viewModel = new ViewModelProvider(getActivity()).get(PlayViewModel.class);
+    // TODO Observe viewModel livedata as appropriate.
   }
 
+  private static class WagerSpanLookup extends SpanSizeLookup {
+
+    @Override
+    public int getSpanSize(int position) {
+      return (position <= 1) ? ZERO_SPACE_WIDTH : NORMAL_SPACE_WIDTH;
+    }
+
+  }
 }
